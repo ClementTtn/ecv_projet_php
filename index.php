189805fd7bbb7f816676f6bfebf2a46ea3e50e71
Clément Tutin <clement.tutin.pro@gmail.com>
2024-02-12 20:15:44 +0100

    Init structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projet PHP</title>
+        <link rel="stylesheet" href="styles/styles.css">
+    </head>
+    <body>
+        <p>Hello world</p>
+    </body>
+</html>
