e0f1c458fc672cd5aae824c2c3d18c5e2d25f591
Clément Tutin <clement.tutin.pro@gmail.com>
2024-02-13 21:42:56 +0100

    Affichage de l'ensemble des produits

--- html/index.php
+++ html/index.php
@@ -1,3 +1,24 @@
 <?php
-require_once('parts/header.php');
+require_once dirname(__FILE__) . '/parts/header.php';
+require_once dirname(__FILE__) . '/models/Product.php';
+
+$listProducts = Product::getListProduct();
 ?>
+
+<h2>Catalogue des produits</h2>
+
+<?php if (empty($listProducts)) : ?>
+<p>Le catalogue est vide</p>
+
+<?php else : ?>
+    <?php foreach ($listProducts as $product) : ?>
+        <article>
+            <p><?php echo $product['id'] ?></p>
+            <p><?php echo $product['name'] ?></p>
+            <p><?php echo $product['description'] ?></p>
+            <p><?php echo $product['price'] ?></p>
+        </article>
+        <hr>
+    <?php endforeach ?>
+<?php endif; ?>
+
